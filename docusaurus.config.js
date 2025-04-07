ad3d89e42a0060de0aa34733bcfa1dd886402473
Damian Kurkowicz <kurkovsw@icloud.com>
2025-04-07 22:35:34 +0200

    add proper configuration

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -23,9 +23,9 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: "facebook", // Usually your GitHub org/user name.
-  projectName: "docusaurus", // Usually your repo name.
-
+  organizationName: "Kurkov11", // Usually your GitHub org/user name.
+  projectName: "sapuniko-docs", // Usually your repo name.
+  trailingSlash: false,
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
 
